bd267d228a99914db134549d681182f54c0c5d0e
Mike Churvis <mikechurvis@gmail.com>
2022-09-22 18:35:42 -0400

    DjangoEssentialsTraining: update home

--- DjangoEssentialTraining/home/views.py
+++ DjangoEssentialTraining/home/views.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'home/welcome.html', {"today": datetime.today()})
+
+
+@login_required(login_url="/admin")
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
